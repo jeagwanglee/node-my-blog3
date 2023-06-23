aece7cd7033d37dfc0f28a3b9768d06529e1e74c
jegwlee <jegwlee@gmail.com>
2023-06-23 15:51:12 +0900

    [Modify]  회원가입 API sequelize로 수정

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../schemas/user.js');
+const { Users } = require('../models');
 const pattern = /^[a-zA-Z0-9]+$/;
 
 // 회원가입 API
 router.post('/signup', async (req, res) => {
   try {
     const { nickname, password, confirm } = req.body;
+    console.log(`nickname : ${nickname}`);
 
-    const existUser = await User.find({ nickname });
+    const existUser = await Users.findOne({
+      where: {
+        nickname,
+      },
+    });
 
     // 닉네임 형식 (3자이상, 알파벳 대소문자, 숫자로 구성하기 (이미 존재하는 닉네임과 중복되면 안됨)
     // 문자열 형식 검사
-    if (nickname.length < 3 || !pattern.test(nickname)) {
+    if (password.length < 3 || !pattern.test(nickname)) {
       return res.status(412).json({ errorMessage: '닉네임의 형식이 일치하지 않습니다.' });
     }
+
     // 비밀번호 일치 확인
     if (password !== confirm) {
       return res.status(412).json({ errorMessage: '패스워드가 일치하지 않습니다.' });
@@ -24,18 +30,19 @@ router.post('/signup', async (req, res) => {
     if (password.length < 4) {
       return res.status(412).json({ errorMessage: '패스워드 형식이 일치하지 않습니다.' });
     }
+
     // 패스워드에 닉네임 포함
     if (password.includes(nickname)) {
       return res.status(412).json({ errorMessage: '패스워드에 닉네임이 포함되어 있습니다.' });
     }
 
     // 닉네임 중복 (이미 DB에 존재하는 닉네임)
-    if (existUser.length) {
+    if (existUser) {
       return res.status(412).json({ errorMessage: '중복된 닉네임입니다.' });
     }
 
     // DB에 회원 저장
-    const user = new User({ nickname, password });
+    const user = new Users({ nickname, password });
     await user.save();
 
     res.status(201).json({ message: '회원가입에 성공하였습니다.' });
