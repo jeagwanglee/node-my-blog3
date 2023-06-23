c92b5fd7e23a54c305e8be8d6f13a1f5f17c0656
jegwlee <jegwlee@gmail.com>
2023-06-23 12:13:11 +0900

    [Rename] 라우트 이름 변경

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const postsRouter = require('./posts.js');
-const usersRouter = require('../routes/users.js');
+const postsRouter = require('./posts.route.js');
+const usersRouter = require('./users.route.js');
 const authRouter = require('./auth.js');
 
 router.use('/posts', postsRouter);
